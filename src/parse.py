9cb19e9cfc43b670731496a41e7da10604cd4578
Dongheon Han <henongod@gmail.com>
2025-11-25 06:14:16 +0000

    Extract only relevant data from json in parse.run_conda_cmd()

--- src/parse.py
+++ src/parse.py
@@ -1,6 +1,7 @@
 import subprocess
 import pathlib
 import re
+import json
 
 def load_reqs_txt(path):
     deps = []
@@ -15,14 +16,24 @@ def load_reqs_txt(path):
 def run_conda_cmd(package):
     try:
         result = subprocess.run(
-            ["conda", "search", package, "--info", "--json"],
+            ["conda", "run", "-n", "base", "conda", "search", package, "--info", "--json"],
             capture_output=True,
             text=True,
         )
-        return json.loads(result.stdout).get(package, [])
     except:
         print(f"[ERROR] conda search: {package} failed")
         return []
+    
+    # print(result.stdout, result.stderr)
+    data = json.loads(result.stdout)
+    extracted_data = [{
+        'version': x['version'], 
+        'depends': x['depends'], 
+        'constrains': x['constrains'],
+    } for x in data[package]]
+
+    # print(extracted_data)
+    return extracted_data
 
 def get_dep_space(requirements):
     space = {}
